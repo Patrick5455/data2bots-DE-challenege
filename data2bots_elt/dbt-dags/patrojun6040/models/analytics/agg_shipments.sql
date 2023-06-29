b52be4dd80b2cdf2c134d106904da37197d73f4d
Patrick Ojunde <patrickojunde@gmail.com>
2023-06-29 15:00:41 +0100

    agg_shipments query

--- data2bots_elt/dbt-dags/patrojun6040/models/analytics/agg_shipments.sql
+++ data2bots_elt/dbt-dags/patrojun6040/models/analytics/agg_shipments.sql
@@ -1,18 +1,18 @@
 DROP TABLE IF EXISTS patrojun6040_analytics.agg_shipments;
 CREATE TABLE patrojun6040_analytics.agg_shipments AS (
-    SELECT
-                count(shipment_id)
-                FILTER (
-                    WHERE shipment_date >= order_date + interval '6 days'
-                    AND delivery_date IS NULL
-                    ) AS tt_late_shipments,
-                count(shipment_id)
+    SELECT current_date AS ingestion_date,
+           count(shipment_id)
+               FILTER (
+                   WHERE shipment_date >= order_date + interval '6 days'
+                             AND delivery_date IS NULL
+                   ) AS tt_late_shipments,
+            count(shipment_id)
                 FILTER (
                     WHERE delivery_date IS NULL
-                    AND shipment_date IS NULL
-                    AND '2022-09-05' = order_date + interval '15 days'
+                              AND shipment_date IS NULL
+                              AND '2022-09-05' = order_date + interval '15 days'
                     ) AS tt_undelievered_shipments
     FROM patrojun6040_staging.shipment_deliveries
-             LEFT JOIN patrojun6040_staging.orders
-                       ON orders.order_id = shipment_deliveries.order_id
+        LEFT JOIN patrojun6040_staging.orders
+            ON orders.order_id = shipment_deliveries.order_id
 );
